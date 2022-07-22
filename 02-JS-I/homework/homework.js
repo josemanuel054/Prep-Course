13e022532c3b2354b0632775d2a3fb07747d1660
unknown <josemanuelordinolavaldez@gmail.com>
2022-07-21 23:07:23 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "9";
+const nuevoNum = "98";
 
 // Crea una variable booleana:
 const nuevoBool = null;
